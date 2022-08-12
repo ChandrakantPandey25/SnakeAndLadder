043b0136c11264cdccc1cb095eaf799da2374885
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 07:19:23 -0700

    Added start position as 0

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder program");
+            StartPosition Start = new StartPosition();
+           
+
+        }
+    }
+}
---
+++ SnakeLadder/SnakeLadder/StartPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    class StartPosition
+    { 
+        public  StartPosition()
+        {
+        Console.WriteLine("Start position is 0");
+            
+        }
+    }
+}
