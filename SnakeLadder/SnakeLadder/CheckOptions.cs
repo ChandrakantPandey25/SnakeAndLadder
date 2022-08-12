13a5f977428ff8adce71edd1f71153cf606343ca
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 07:31:00 -0700

    Added program to Check Options

---
+++ SnakeLadder/SnakeLadder/CheckOptions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    class CheckOptions
+    {
+        public CheckOptions()
+        {
+
+
+            RollDie roll = new RollDie();
+            int newValue = roll.RollDieResult();
+            Console.WriteLine("The new Die Value is:" + newValue);
+            Random random = new Random();
+            int option = random.Next(1, 4);
+            switch (option)
+            {
+
+                case 1:
+                    Console.WriteLine("No play");
+
+                    break;
+                case 2:
+                    Console.WriteLine("you got a Ladder");
+
+
+
+
+                    int newPosition = 0 + newValue;
+                    Console.WriteLine("Your new Position is " + newPosition);
+
+                    break;
+                case 3:
+                    Console.WriteLine("you got a Snake");
+
+
+
+
+                    int newPositionOne = 0 - newValue;
+
+                    if (newPositionOne <= 0)
+                    {
+                        Console.WriteLine("your new position is :" + 0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your new Position is " + newPositionOne);
+                    }
+
+                    break;
+
+
+            }
+
+        }
+    }
+}
--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -9,6 +9,7 @@ namespace SnakeLadder
             Console.WriteLine("Welcome to Snake and Ladder program");
             StartPosition Start = new StartPosition();
             Console.WriteLine("Enter 1 to roll die");
+            Console.WriteLine("Enter 2 to CheckOptions");
             int result = Convert.ToInt32(Console.ReadLine());
             switch(result)
             {
@@ -16,6 +17,10 @@ namespace SnakeLadder
                     RollDie roll = new RollDie();
                     int newValue = roll.RollDieResult();
                     Console.WriteLine("New die value is :" + newValue);
+                    break;
+                case 2:
+
+                    CheckOptions newresult = new CheckOptions();
                     break;
                 default:
                     Console.WriteLine("Enter valid number");
