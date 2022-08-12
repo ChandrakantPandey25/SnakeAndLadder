b4c4079f83b869e5cb6d6781edb509649f859fc1
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 07:24:38 -0700

    Added program to roll the die

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -8,7 +8,20 @@ namespace SnakeLadder
         {
             Console.WriteLine("Welcome to Snake and Ladder program");
             StartPosition Start = new StartPosition();
-           
+            Console.WriteLine("Enter 1 to roll die");
+            int result = Convert.ToInt32(Console.ReadLine());
+            switch(result)
+            {
+                case 1:
+                    RollDie roll = new RollDie();
+                    int newValue = roll.RollDieResult();
+                    Console.WriteLine("New die value is :" + newValue);
+                    break;
+                default:
+                    Console.WriteLine("Enter valid number");
+                    break;
+            }
+
 
         }
     }
---
+++ SnakeLadder/SnakeLadder/RollDie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    class RollDie
+    {
+        public int RollDieResult()
+        {
+
+            Random Roll = new Random();
+            int dieResult = Roll.Next(1, 7);
+
+            return dieResult;
+
+        }
+    }
+}
